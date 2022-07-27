84a8ca6ebc1568f3bbba046c1303db56f6822f3d
Pronyushkin_qa <kollaydereva@gmail.com>
2022-07-27 19:44:37 +0300

    1 коммит

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,14 @@ public class Main {
 
         System.out.println("Задание номер 1");
         int spendingPermonth = 0;
-        for (int i : arr){
-            spendingPermonth += i;
+        for (int j : arr){
+            spendingPermonth += j;
         }
         System.out.println("сумма трат за месяц составила " + spendingPermonth + " рублей");
+
         System.out.println("задание номер 2");
 
-        int maxSpending = -1;
+        int maxSpending = 99999;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxSpending) {
                 maxSpending = arr[i];
@@ -34,7 +35,7 @@ public class Main {
 
         }
         System.out.println("максимальная сумма трат за день составила " + maxSpending + " рублей");
-        int minSpending = 5000000;
+        int minSpending = 200001;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < minSpending){
                 minSpending = arr[i];
@@ -42,16 +43,32 @@ public class Main {
 
         }
         System.out.println("минимальная сумма трат за день составила " + minSpending + " рублей");
+
         System.out.println("Задание номер 3");
-        float amountOfDays = 31;
-        float averageSpending = spendingPermonth / amountOfDays ;
-        System.out.println("средняя сумма трат за месяц составила " + averageSpending + " рублей");
+
+        double amountOfDays = (double) spendingPermonth / arr.length;
+        System.out.println("средняя сумма трат за месяц составила " + amountOfDays + " рублей");
+
         System.out.println("Задание номер 4");
+
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length -1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
 
         }
+        // Переворачиваем массив получается ( хз то я сделаль или нет,если что Насяльника не ругайся ) :)))
+        char n = (char) reverseFullName.length;
+        for (int i = 0; i < n/2;i++){
+            char temp = reverseFullName[n - i - 1];
+            reverseFullName[n - i - 1] = reverseFullName[i];
+            reverseFullName[i] = temp;
+        }
+        System.out.println();
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[i]);
+
+        }
+
 
 
 
